b21db3300b6775752e02c0aa8ee07fd12b06c901
Anna Vasileva <vasilieva.a@icloud.com>
2024-06-28 12:27:46 -0400

    display weather.temp

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -2,12 +2,18 @@ import "./Header.css";
 import logo from "../../assets/Logo.svg";
 import avatar from "../../assets/Avatar.png";
 
-function Header() {
+function Header({handleAddClick, weatherData}) {
+
+  const currentDate = new Date().toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+  });
+
   return (
     <header className="header">
         <img className="header__logo" src={logo} />
-        <p className="header__date-and-location">Date, location</p>
-      <button className="header__button">+ Add clothes</button>
+        <p className="header__date-and-location">{currentDate}, {weatherData.city}</p>
+      <button onClick={handleAddClick} type="button" className="header__button">+ Add clothes</button>
       <div className="header__user-container">
         <p className="header__username">Terrence Tegegne</p>
         <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
