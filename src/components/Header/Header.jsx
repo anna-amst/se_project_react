5b3cb4fc523c97b33e4193c6654182b725f86c3e
Anna Vasileva <vasilieva.a@icloud.com>
2024-07-21 14:35:20 -0400

    add toggle switch

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 import logo from "../../assets/Logo.svg";
 import avatar from "../../assets/Avatar.png";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 
 function Header({handleAddClick, weatherData}) {
 
@@ -13,8 +14,10 @@ function Header({handleAddClick, weatherData}) {
     <header className="header">
         <img className="header__logo" alt="logo" src={logo} />
         <p className="header__date-and-location">{currentDate}, {weatherData.city}</p>
+        <ToggleSwitch />
       <button onClick={handleAddClick} type="button" className="header__button">+ Add clothes</button>
       <div className="header__user-container">
+        
         <p className="header__username">Terrence Tegegne</p>
         <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
       </div>
---
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./ToggleSwitch.css";
+import { useState } from "react";
+
+const ToggleSwitch = () => {
+  const [currentTempUnit, handleToggleSwitchChange] = useState("C");
+  const handleChange = () => {
+    if (currentTempUnit === "C") handleToggleSwitchChange("F");
+    if (currentTempUnit === "F") handleToggleSwitchChange("C");
+  };
+
+  return (
+    <label className="switch">
+      <input type="checkbox" className="switch__box" onChange={handleChange} />
+      <span
+        className={
+          currentTempUnit === "F"
+            ? "switch__slider switch__slider-F"
+            : "switch__slider switch__slider-C"
+        }
+      ></span>
+      <p
+        className={`switch__temp-F ${
+          currentTempUnit === "F" && "switch__active"
+        }`}
+      >
+        F
+      </p>
+      <p
+        className={`switch__temp-C ${
+          currentTempUnit === "C" && "switch__active"
+        }`}
+      >
+        C
+      </p>
+    </label>
+  );
+};
+
+export default ToggleSwitch;
